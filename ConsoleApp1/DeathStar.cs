53d9d7629ba8e11abfbdcc2233756725e04318be
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-19 11:19:56 -0800

    Add project files.

---
+++ ConsoleApp1/DeathStar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DeathStar: SpaceStation
+    {
+      
+            public override void FireLaser()
+            {
+                Console.WriteLine("Pew pew");
+            }
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeathStar d = new DeathStar();
+            d.FireLaser();
+        }
+    }
+     
+   
+    
+}
